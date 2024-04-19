79fc7a2b454a0135f760250169d68107abfe5edd
yusuf <yusufgad24@gmail.com>
2024-04-19 19:46:29 +0300

    Filter invoices with url state

--- client/src/ui/Filter.jsx
+++ client/src/ui/Filter.jsx
@@ -1,12 +1,28 @@
 import { useEffect, useRef, useState } from "react";
 import iconDown from "../../public/assets/icon-arrow-down.svg";
 import iconCheck from "../../public/assets/icon-check.svg";
+import { useSearchParams } from "react-router-dom";
 
 function Filter() {
+  const [activeFilters, setActiveFilters] = useSearchParams();
   const [isActive, setIsActive] = useState(false);
 
   const menu = useRef();
   const button = useRef();
+
+  function handleSelect(e) {
+    const filterElement = e.target.closest(".filter");
+
+    if (filterElement) {
+      const filterText = filterElement.textContent.toLowerCase();
+
+      filterElement.firstElementChild.checked
+        ? activeFilters.set(filterText, true)
+        : activeFilters.delete(filterText);
+
+      setActiveFilters(activeFilters);
+    }
+  }
 
   useEffect(() => {
     function handleClickOutside(event) {
@@ -35,15 +51,23 @@ function Filter() {
       >
         Filter
         <span className="ml-1 hidden md:inline-block">by status</span>
-        <img className="ml-2" src={iconDown} alt="icon down" />
+        <img
+          className={`${isActive ? "rotate-180" : "rotate-0"} transition-2 ml-2`}
+          src={iconDown}
+          alt="icon down"
+        />
       </button>
 
       <ul
         ref={menu}
         className={`${isActive ? "block" : "hidden"} absolute right-0 z-30 mt-4 w-48 space-y-3 rounded-md bg-skin-white p-6`}
       >
-        <li className=" group flex select-none items-center">
+        <li
+          onClick={handleSelect}
+          className="group flex select-none items-center filter"
+        >
           <input
+            checked={activeFilters.get("draft")}
             className="transition-1 peer 
             relative h-5 w-5 shrink-0 cursor-pointer appearance-none rounded-sm border-2 border-transparent bg-skin-gray/10  checked:border-0
             checked:bg-skin-purple group-hover:border-skin-purple dark:bg-skin-vulcan"
@@ -63,8 +87,12 @@ function Filter() {
           />
         </li>
 
-        <li className="group flex select-none items-center">
+        <li
+          onClick={handleSelect}
+          className="group flex select-none items-center filter"
+        >
           <input
+            checked={activeFilters.get("pending")}
             className="transition-1 peer relative 
             h-5 w-5 shrink-0 cursor-pointer appearance-none rounded-sm border-2 border-transparent bg-skin-gray/10  checked:border-0
             checked:bg-skin-purple group-hover:border-skin-purple dark:bg-skin-vulcan"
@@ -83,8 +111,13 @@ function Filter() {
             alt="check icon"
           />
         </li>
-        <li className="group flex select-none items-center">
+
+        <li
+          onClick={handleSelect}
+          className="group flex select-none items-center filter"
+        >
           <input
+            checked={activeFilters.get("paid")}
             className="transition-1 peer relative 
             h-5 w-5 shrink-0 cursor-pointer appearance-none rounded-sm border-2 border-transparent bg-skin-gray/10  checked:border-0
             checked:bg-skin-purple group-hover:border-skin-purple dark:bg-skin-vulcan"
